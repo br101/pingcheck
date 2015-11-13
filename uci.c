02938d0abee5b86e8a9cfe88d6228c8dc97450a3
Bruno Randolf <br1@einfach.org>
2015-11-13 14:51:22 +0000

    Add GPLv2 header to all files
    This program is licensed under the GPLv2.

--- uci.c
+++ uci.c
@@ -1,3 +1,17 @@
+/* pingcheck - Check connectivity of interfaces in OpenWRT
+ *
+ * Copyright (C) 2015 Bruno Randolf <br1@einfach.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
