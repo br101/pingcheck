0f099998782f550e2abebdc65bcc3e969b798769
Bruno Randolf <br1@einfach.org>
2015-11-19 20:53:52 +0000

    Include stddef.h before uci.h redefines offsetof

--- uci.c
+++ uci.c
@@ -12,6 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
