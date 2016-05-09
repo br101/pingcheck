05176502e90cbfc289517beb7780b6d34ca8ae10
Bruno Randolf <br1@einfach.org>
2016-05-09 16:21:12 +0100

    pass global state to scripts

--- scripts.c
+++ scripts.c
@@ -45,9 +45,9 @@ static void task_scripts_run(struct runqueue *q, struct runqueue_task *t)
 
 	/* child process */
 	len = snprintf(cmd, sizeof(cmd),
-		       "export INTERFACE=\"%s\"; export DEVICE=\"%s\"; "
+		       "export INTERFACE=\"%s\"; export DEVICE=\"%s\"; export GLOBAL=\"%s\"; "
 		       "for hook in /etc/pingcheck/%s.d/*; do [ -r \"$hook\" ] && sh $hook; done",
-		       pi->name, pi->device, state_str);
+		       pi->name, pi->device, get_global_status_str(), state_str);
 
 	if (len <= 0 || (unsigned int)len >= sizeof(cmd)) { // error or truncated
 		printlog(LOG_ERR, "Run scripts commands truncated!");
