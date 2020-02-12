520718f9377eab49888a3e38ece59f9ad94d978e
Bruno Randolf <br1@einfach.org>
2020-02-12 11:42:12 +0000

    Add example script

---
+++ example-script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# ${INTERFACE} -- logical network interface (e.g. wan) which goes online or offline
+# ${DEVICE} -- physical device (e.g. eth0) which goes online or offline
+# ${GLOBAL} -- global state ONLINE or OFFLINE depending on wether device is online thru other interfaces
