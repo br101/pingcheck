br101/pingcheck

commit 8cee8ac5bece8f07b0770a2f239a4738d0526bd1

Add curly braces and reformat

icmp.c
@@ -12,30 +12,32 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+#include "main.h"
+#include <err.h>
+#include <linux/icmp.h>
+#include <linux/ip.h>
+#include <net/if.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <linux/ip.h>
-#include <linux/icmp.h>
-#include <net/if.h>
-#include <err.h>
-#include "main.h"
+#include <unistd.h>
 
 static int pid = -1;
 
 /* standard 1s complement checksum */
-static unsigned short checksum(void *b, int len)
+static unsigned short checksum(void* b, int len)
 {
-	unsigned short *buf = b;
+	unsigned short* buf = b;
 	unsigned int sum = 0;
 	unsigned short result;
 
-	for (sum = 0; len > 1; len -= 2)
+	for (sum = 0; len > 1; len -= 2) {
 		sum += *buf++;
-	if (len == 1)
+	}
+	if (len == 1) {
 		sum += *(unsigned char*)buf;
+	}
 	sum = (sum >> 16) + (sum & 0xFFFF);
 	sum += (sum >> 16);
 	result = ~sum;
@@ -89,7 +91,8 @@ bool icmp_echo_send(int fd, int dst_ip, int cnt)
 	icmp->checksum = 0;
 	icmp->checksum = checksum(buf, sizeof(struct icmphdr));
 
-	ret = sendto(fd, &buf, sizeof(struct icmphdr), 0, (struct sockaddr*)&addr, sizeof(addr));
+	ret = sendto(fd, &buf, sizeof(struct icmphdr), 0, (struct sockaddr*)&addr,
+				 sizeof(addr));
 	if (ret <= 0) {
 		warn("sendto");
 		return false;
@@ -108,16 +111,16 @@ bool icmp_echo_receive(int fd)
 		return false;
 	}
 
-	struct iphdr *ip = (struct iphdr*)buf;
-	struct icmphdr *icmp = (struct icmphdr*)(buf + ip->ihl*4);
+	struct iphdr* ip = (struct iphdr*)buf;
+	struct icmphdr* icmp = (struct icmphdr*)(buf + ip->ihl * 4);
 
 	int csum_recv = icmp->checksum;
 	icmp->checksum = 0; // need to zero before calculating checksum
 	int csum_calc = checksum(icmp, sizeof(struct icmphdr));
 
-	if (csum_recv == csum_calc &&		// checksum correct
-	    icmp->type == ICMP_ECHOREPLY &&	// correct type
-	    ntohs(icmp->un.echo.id) == pid) {	// we are sender
+	if (csum_recv == csum_calc &&		  // checksum correct
+		icmp->type == ICMP_ECHOREPLY &&	  // correct type
+		ntohs(icmp->un.echo.id) == pid) { // we are sender
 		return true;
 	}
 	return false;

.clang-format
@@ -1,0 +1,18 @@
+BasedOnStyle: LLVM
+Language: Cpp
+UseTab: Always
+IndentWidth: 4
+TabWidth: 4
+AccessModifierOffset: -4
+ColumnLimit: 80
+BreakBeforeBraces: Linux
+AllowShortIfStatementsOnASingleLine: false
+AllowShortFunctionsOnASingleLine: InlineOnly
+IndentCaseLabels: false
+BreakBeforeBinaryOperators: true
+AlignConsecutiveMacros: true
+
+# Force pointers to the type for C++.
+DerivePointerAlignment: false
+PointerAlignment: Left
+

log.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <syslog.h>
 
 #include "log.h"
 
-void __attribute__ ((format (printf, 2, 3)))
-log_out(enum loglevel level, const char *format, ...)
+void __attribute__((format(printf, 2, 3)))
+log_out(enum loglevel level, const char* format, ...)
 {
 	va_list args;
 
@@ -23,9 +23,9 @@ log_out(enum loglevel level, const char *format, ...)
 	}
 }
 
-void log_open(const char *name)
+void log_open(const char* name)
 {
-	openlog(name, LOG_PID|LOG_CONS, LOG_DAEMON);
+	openlog(name, LOG_PID | LOG_CONS, LOG_DAEMON);
 }
 
 void log_close(void)

log.h
@@ -5,9 +5,9 @@
 enum loglevel { LL_CRIT = 2, LL_ERR, LL_WARN, LL_NOTICE, LL_INFO, LL_DEBUG };
 
 /* application needs to provide */
-void __attribute__ ((format (printf, 2, 3)))
-log_out(enum loglevel, const char *fmt, ...);
-void log_open(const char *name);
+void __attribute__((format(printf, 2, 3)))
+log_out(enum loglevel, const char* fmt, ...);
+void log_open(const char* name);
 void log_close(void);
 
 #ifndef DEBUG
@@ -18,10 +18,14 @@ void log_close(void);
 #undef LOG_ERR
 
 #define LOG_CRIT(...) log_out(LL_CRIT, __VA_ARGS__)
-#define LOG_ERR(...) log_out(LL_ERR, __VA_ARGS__)
+#define LOG_ERR(...)  log_out(LL_ERR, __VA_ARGS__)
 #define LOG_WARN(...) log_out(LL_WARN, __VA_ARGS__)
 #define LOG_NOTI(...) log_out(LL_NOTICE, __VA_ARGS__)
-#define LOG_INF(...) log_out(LL_INFO, __VA_ARGS__)
-#define LOG_DBG(...) do { if (DEBUG) log_out(LL_DEBUG, __VA_ARGS__); } while (0)
+#define LOG_INF(...)  log_out(LL_INFO, __VA_ARGS__)
+#define LOG_DBG(...)                                                           \
+	do {                                                                       \
+		if (DEBUG)                                                             \
+			log_out(LL_DEBUG, __VA_ARGS__);                                    \
+	} while (0)
 
 #endif

main.c
@@ -12,12 +12,12 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
 #include "log.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /* main list of interfaces */
 static struct ping_intf intf[MAX_NUM_INTERFACES];
@@ -27,18 +27,21 @@ static struct uloop_timeout timeout_panic;
 
 void state_change(enum online_state state_new, struct ping_intf* pi)
 {
-	if (pi->state == state_new) /* no change */
+	if (pi->state == state_new) { /* no change */
 		return;
+	}
 
 	pi->state = state_new;
 
 	LOG_INF("Interface '%s' changed to %s", pi->name,
-		 get_status_str(pi->state));
+			get_status_str(pi->state));
 
 	enum online_state global_state = get_global_status();
 	if (global_state == OFFLINE) {
-		if (pi->conf_panic_timeout > 0)
-			uloop_timeout_set(&timeout_panic, pi->conf_panic_timeout * 60 * 1000);
+		if (pi->conf_panic_timeout > 0) {
+			uloop_timeout_set(&timeout_panic,
+							  pi->conf_panic_timeout * 60 * 1000);
+		}
 	} else {
 		uloop_timeout_cancel(&timeout_panic);
 	}
@@ -49,21 +52,29 @@ void state_change(enum online_state state_new, struct ping_intf* pi)
 const char* get_status_str(enum online_state state)
 {
 	switch (state) {
-		case UNKNOWN: return "UNKNOWN";
-		case DOWN: return "DOWN";
-		case UP_WITHOUT_DEFAULT_ROUTE: return "UP_WITHOUT_DEFAULT_ROUTE";
-		case UP: return "UP";
-		case OFFLINE: return "OFFLINE";
-		case ONLINE: return "ONLINE";
-		default: return "INVALID";
+	case UNKNOWN:
+		return "UNKNOWN";
+	case DOWN:
+		return "DOWN";
+	case UP_WITHOUT_DEFAULT_ROUTE:
+		return "UP_WITHOUT_DEFAULT_ROUTE";
+	case UP:
+		return "UP";
+	case OFFLINE:
+		return "OFFLINE";
+	case ONLINE:
+		return "ONLINE";
+	default:
+		return "INVALID";
 	}
 }
 
 enum online_state get_global_status(void)
 {
-	for (int i=0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
-		if (intf[i].state == ONLINE)
+	for (int i = 0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
+		if (intf[i].state == ONLINE) {
 			return ONLINE;
+		}
 	}
 	return OFFLINE;
 }
@@ -71,9 +82,10 @@ enum online_state get_global_status(void)
 int get_online_interface_names(const char** dest, int destLen)
 {
 	int j = 0;
-	for (int i=0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
-		if (intf[i].state == ONLINE && j < destLen)
+	for (int i = 0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
+		if (intf[i].state == ONLINE && j < destLen) {
 			dest[j++] = intf[i].name;
+		}
 	}
 	return j;
 }
@@ -91,8 +103,9 @@ int get_all_interface_names(const char** dest, int destLen)
 void notify_interface(const char* interface, const char* action)
 {
 	struct ping_intf* pi = get_interface(interface);
-	if (pi == NULL)
+	if (pi == NULL) {
 		return;
+	}
 
 	if (strcmp(action, "ifup") == 0) {
 		LOG_INF("Interface '%s' event UP", interface);
@@ -110,7 +123,7 @@ struct ping_intf* get_interface(const char* interface)
 {
 	struct ping_intf* pi = NULL;
 	/* find interface in our list */
-	for (int i=0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
+	for (int i = 0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
 		if (strncmp(intf[i].name, interface, MAX_IFNAME_LEN) == 0) {
 			pi = &intf[i];
 			break;
@@ -120,9 +133,11 @@ struct ping_intf* get_interface(const char* interface)
 }
 
 /* reset counters for all (pass NULL) or one interface */
-void reset_counters(const char* interface) {
-	for (int i=0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
-		if (interface == NULL || strncmp(intf[i].name, interface, MAX_IFNAME_LEN) == 0) {
+void reset_counters(const char* interface)
+{
+	for (int i = 0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
+		if (interface == NULL
+			|| strncmp(intf[i].name, interface, MAX_IFNAME_LEN) == 0) {
 			intf[i].cnt_sent = 0;
 			intf[i].cnt_succ = 0;
 			intf[i].last_rtt = 0;
@@ -132,7 +147,7 @@ void reset_counters(const char* interface) {
 }
 
 /* uloop timeout callback when offline for too long */
-static void uto_panic_cb(__attribute__((unused)) struct uloop_timeout *t)
+static void uto_panic_cb(__attribute__((unused)) struct uloop_timeout* t)
 {
 	scripts_run_panic();
 }
@@ -158,8 +173,9 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char** argv)
 	scripts_init();
 
 	ret = ubus_init();
-	if (!ret)
+	if (!ret) {
 		goto exit;
+	}
 
 	/* listen for ubus network interface events */
 	ret = ubus_listen_network_events();
@@ -171,25 +187,27 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char** argv)
 	ubus_register_server();
 
 	/* start ping on all available interfaces */
-	for (int i=0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
+	for (int i = 0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
 		ping_init(&intf[i]);
 	}
 
 	/* initialize panic handler */
 	timeout_panic.cb = uto_panic_cb;
-	if (intf[0].conf_panic_timeout > 0)
-		uloop_timeout_set(&timeout_panic, intf[0].conf_panic_timeout * 60 * 1000);
+	if (intf[0].conf_panic_timeout > 0) {
+		uloop_timeout_set(&timeout_panic,
+						  intf[0].conf_panic_timeout * 60 * 1000);
+	}
 
 	/* main loop */
 	uloop_run();
 
 	/* print statistics and cleanup */
 	printf("\n");
-	for (int i=0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
+	for (int i = 0; i < MAX_NUM_INTERFACES && intf[i].name[0]; i++) {
 		printf("%s:\t%-8s %3.0f%% (%d/%d on %s)\n", intf[i].name,
-		       get_status_str(intf[i].state),
-		       (float)intf[i].cnt_succ*100/intf[i].cnt_sent,
-		       intf[i].cnt_succ, intf[i].cnt_sent, intf[i].device);
+			   get_status_str(intf[i].state),
+			   (float)intf[i].cnt_succ * 100 / intf[i].cnt_sent,
+			   intf[i].cnt_succ, intf[i].cnt_sent, intf[i].device);
 		ping_stop(&intf[i]);
 	}
 

main.h
@@ -12,17 +12,24 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#include <stdbool.h>
-#include <libubox/uloop.h>
 #include <libubox/runqueue.h>
+#include <libubox/uloop.h>
+#include <stdbool.h>
 
-#define MAX_IFNAME_LEN		256
-#define MAX_HOSTNAME_LEN	256
-#define MAX_NUM_INTERFACES	8
-#define SCRIPTS_TIMEOUT		10 /* 10 sec */
-#define UBUS_TIMEOUT		3000 /* 3 sec */
+#define MAX_IFNAME_LEN	   256
+#define MAX_HOSTNAME_LEN   256
+#define MAX_NUM_INTERFACES 8
+#define SCRIPTS_TIMEOUT	   10	/* 10 sec */
+#define UBUS_TIMEOUT	   3000 /* 3 sec */
 
-enum online_state { UNKNOWN, DOWN, UP_WITHOUT_DEFAULT_ROUTE, UP, OFFLINE, ONLINE };
+enum online_state {
+	UNKNOWN,
+	DOWN,
+	UP_WITHOUT_DEFAULT_ROUTE,
+	UP,
+	OFFLINE,
+	ONLINE
+};
 
 enum protocol { ICMP, TCP };
 
@@ -39,17 +46,17 @@ struct ping_intf {
 	enum online_state state;
 	unsigned int cnt_sent;
 	unsigned int cnt_succ;
-	unsigned int last_rtt;	/* in ms */
-	unsigned int max_rtt;	/* in ms */
+	unsigned int last_rtt; /* in ms */
+	unsigned int max_rtt;  /* in ms */
 
 	/* config items */
 	int conf_interval;
 	int conf_timeout;
 	char conf_hostname[MAX_HOSTNAME_LEN];
-	int conf_host;		/* resolved IP */
+	int conf_host; /* resolved IP */
 	enum protocol conf_proto;
 	int conf_tcp_port;
-	int conf_panic_timeout;	/* minutes */
+	int conf_panic_timeout; /* minutes */
 
 	/* internal state for ping */
 	struct uloop_fd ufd;
@@ -82,7 +89,8 @@ void ping_stop(struct ping_intf* pi);
 // ubus.c
 bool ubus_init(void);
 bool ubus_listen_network_events(void);
-int ubus_interface_get_status(const char* name, char* device, size_t device_len);
+int ubus_interface_get_status(const char* name, char* device,
+							  size_t device_len);
 bool ubus_register_server(void);
 void ubus_finish(void);
 

ping.c
@@ -12,17 +12,17 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#include <stdio.h>
-#include <unistd.h>
-#include <time.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include "main.h"
 #include "log.h"
+#include "main.h"
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
 
-static void ping_uloop_fd_close(struct uloop_fd *ufd)
+static void ping_uloop_fd_close(struct uloop_fd* ufd)
 {
 	if (ufd != NULL && ufd->fd > 0) {
 		uloop_fd_delete(ufd);
@@ -32,58 +32,63 @@ static void ping_uloop_fd_close(struct uloop_fd *ufd)
 }
 
 /* uloop callback when received something on a ping socket */
-static void ping_fd_handler(struct uloop_fd *fd,
-			    __attribute__((unused)) unsigned int events)
+static void ping_fd_handler(struct uloop_fd* fd,
+							__attribute__((unused)) unsigned int events)
 {
 	struct ping_intf* pi = container_of(fd, struct ping_intf, ufd);
 
 	if (pi->conf_proto == ICMP) {
-		if (!icmp_echo_receive(fd->fd))
+		if (!icmp_echo_receive(fd->fd)) {
 			return;
+		}
 	} else if (pi->conf_proto == TCP) {
 		/* with TCP, the handler is called when connect() succeds or fails.
-		*
-		* if the connect takes longer than the ping interval, it is timed
-		* out and assumed failed before we open the next regular connection,
-		* and this handler is not called. but if the interval is large and
-		* in other cases, this handler can be called for failed connections,
-		* and to be sure we need to check if connect was successful or not.
-		*
-		* after that we just close the socket, as we don't need to send or
-		* receive any data */
+		 *
+		 * if the connect takes longer than the ping interval, it is timed
+		 * out and assumed failed before we open the next regular connection,
+		 * and this handler is not called. but if the interval is large and
+		 * in other cases, this handler can be called for failed connections,
+		 * and to be sure we need to check if connect was successful or not.
+		 *
+		 * after that we just close the socket, as we don't need to send or
+		 * receive any data */
 		bool succ = tcp_check_connect(fd->fd);
 		ping_uloop_fd_close(fd);
-		//printf("TCP connected %d\n", succ);
-		if (!succ)
+		// printf("TCP connected %d\n", succ);
+		if (!succ) {
 			return;
+		}
 	}
 
-	//LOG_DBG("Received pong on '%s'", pi->name);
+	// LOG_DBG("Received pong on '%s'", pi->name);
 	pi->cnt_succ++;
 
 	/* calculate round trip time */
 	struct timespec time_recv;
 	clock_gettime(CLOCK_MONOTONIC, &time_recv);
 	pi->last_rtt = timespec_diff_ms(pi->time_sent, time_recv);
-	if (pi->last_rtt > pi->max_rtt)
+	if (pi->last_rtt > pi->max_rtt) {
 		pi->max_rtt = pi->last_rtt;
+	}
 
 	/* online just confirmed: move timeout for offline to later
 	 * and give the next reply an extra window of two times the last RTT */
-	uloop_timeout_set(&pi->timeout_offline, pi->conf_timeout * 1000 + pi->last_rtt * 2);
+	uloop_timeout_set(&pi->timeout_offline,
+					  pi->conf_timeout * 1000 + pi->last_rtt * 2);
 
 	state_change(ONLINE, pi);
 }
 
-/* uloop timeout callback when we did not receive a ping reply for a certain time */
-static void uto_offline_cb(struct uloop_timeout *t)
+/* uloop timeout callback when we did not receive a ping reply for a certain
+ * time */
+static void uto_offline_cb(struct uloop_timeout* t)
 {
 	struct ping_intf* pi = container_of(t, struct ping_intf, timeout_offline);
 	state_change(OFFLINE, pi);
 }
 
 /* uloop timeout callback when it's time to send a ping */
-static void uto_ping_send_cb(struct uloop_timeout *t)
+static void uto_ping_send_cb(struct uloop_timeout* t)
 {
 	struct ping_intf* pi = container_of(t, struct ping_intf, timeout_send);
 	ping_send(pi);
@@ -111,28 +116,28 @@ bool ping_init(struct ping_intf* pi)
 		return false;
 	} else if (ret == 1) {
 		LOG_INF("Interface '%s' (%s) has no default route but local one",
-			pi->name, pi->device);
+				pi->name, pi->device);
 		pi->state = UP_WITHOUT_DEFAULT_ROUTE;
 	} else if (ret == 2) {
 		pi->state = UP;
 	}
 
-	LOG_INF("Init %s ping on '%s' (%s)",
-		 pi->conf_proto == TCP ? "TCP" : "ICMP", pi->name, pi->device);
+	LOG_INF("Init %s ping on '%s' (%s)", pi->conf_proto == TCP ? "TCP" : "ICMP",
+			pi->name, pi->device);
 
 	/* init ICMP socket. for TCP we open a new socket every time */
 	if (pi->conf_proto == ICMP) {
 		ret = icmp_init(pi->device);
-		if (ret < 0)
+		if (ret < 0) {
 			return false;
+		}
 
 		/* add socket handler to uloop */
 		pi->ufd.fd = ret;
 		pi->ufd.cb = ping_fd_handler;
 		ret = uloop_fd_add(&pi->ufd, ULOOP_READ);
 		if (ret < 0) {
-			LOG_ERR("Could not add uloop fd %d for '%s'",
-				pi->ufd.fd, pi->name);
+			LOG_ERR("Could not add uloop fd %d for '%s'", pi->ufd.fd, pi->name);
 			return false;
 		}
 	}
@@ -141,8 +146,7 @@ bool ping_init(struct ping_intf* pi)
 	pi->timeout_send.cb = uto_ping_send_cb;
 	ret = uloop_timeout_set(&pi->timeout_send, 1000);
 	if (ret < 0) {
-		LOG_ERR("Could not add uloop send timeout for '%s'",
-			 pi->name);
+		LOG_ERR("Could not add uloop send timeout for '%s'", pi->name);
 		return false;
 	}
 
@@ -153,10 +157,10 @@ bool ping_init(struct ping_intf* pi)
 	 * interval. this will later be adjusted to the last RTT
 	 */
 	pi->timeout_offline.cb = uto_offline_cb;
-	ret = uloop_timeout_set(&pi->timeout_offline, pi->conf_timeout * 1000 + 900);
+	ret = uloop_timeout_set(&pi->timeout_offline,
+							pi->conf_timeout * 1000 + 900);
 	if (ret < 0) {
-		LOG_ERR("Could not add uloop offline timeout for '%s'",
-			 pi->name);
+		LOG_ERR("Could not add uloop offline timeout for '%s'", pi->name);
 		return false;
 	}
 
@@ -176,7 +180,7 @@ static void ping_resolve(struct ping_intf* pi)
 
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
-	hints.ai_socktype = pi->conf_proto == ICMP? SOCK_DGRAM : SOCK_STREAM;
+	hints.ai_socktype = pi->conf_proto == ICMP ? SOCK_DGRAM : SOCK_STREAM;
 
 	int r = getaddrinfo(pi->conf_hostname, NULL, &hints, &addr);
 	if (r < 0 || addr == NULL) {
@@ -187,7 +191,7 @@ static void ping_resolve(struct ping_intf* pi)
 	/* use only first address */
 	struct sockaddr_in* sa = (struct sockaddr_in*)addr->ai_addr;
 	printf("Resolved %s to %s\n", pi->conf_hostname,
-	       inet_ntoa((struct in_addr)sa->sin_addr));
+		   inet_ntoa((struct in_addr)sa->sin_addr));
 	pi->conf_host = sa->sin_addr.s_addr;
 
 	freeaddrinfo(addr);
@@ -200,7 +204,7 @@ static void ping_resolve(struct ping_intf* pi)
 static bool ping_send_tcp(struct ping_intf* pi)
 {
 	if (pi->ufd.fd > 0) {
-		//LOG_DBG("TCP connection timed out '%s'", pi->name);
+		// LOG_DBG("TCP connection timed out '%s'", pi->name);
 		ping_uloop_fd_close(&pi->ufd);
 	}
 
@@ -212,8 +216,7 @@ static bool ping_send_tcp(struct ping_intf* pi)
 		pi->ufd.cb = ping_fd_handler;
 		ret = uloop_fd_add(&pi->ufd, ULOOP_WRITE);
 		if (ret < 0) {
-			LOG_ERR("Could not add uloop fd %d for '%s'",
-				pi->ufd.fd, pi->name);
+			LOG_ERR("Could not add uloop fd %d for '%s'", pi->ufd.fd, pi->name);
 			return false;
 		}
 	}
@@ -225,8 +228,9 @@ bool ping_send(struct ping_intf* pi)
 	bool ret = false;
 
 	/* resolve at least every 10th time */
-	if (pi->conf_host == 0 || pi->state != ONLINE || pi->cnt_sent % 10 == 0)
+	if (pi->conf_host == 0 || pi->state != ONLINE || pi->cnt_sent % 10 == 0) {
 		ping_resolve(pi);
+	}
 
 	/* either send ICMP ping or start TCP connection */
 	if (pi->conf_proto == ICMP) {

scripts.c
@@ -12,11 +12,11 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "main.h"
 #include "log.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /* run queue for scripts */
 static struct runqueue runq;
@@ -28,7 +28,7 @@ static struct runqueue_process proc_panic;
  * Here we fork and run the scripts in the child process.
  * The parent process just monitors the child process.
  */
-static void task_scripts_run(struct runqueue *q, struct runqueue_task *t)
+static void task_scripts_run(struct runqueue* q, struct runqueue_task* t)
 {
 	char cmd[500];
 	int len = 0;
@@ -47,10 +47,12 @@ static void task_scripts_run(struct runqueue *q, struct runqueue_task *t)
 	}
 
 	/* child process */
-	len = snprintf(cmd, sizeof(cmd),
-		       "export INTERFACE=\"%s\"; export DEVICE=\"%s\"; export GLOBAL=\"%s\"; "
-		       "for hook in /etc/pingcheck/%s.d/*; do [ -r \"$hook\" ] && sh $hook; done",
-		       pi->name, pi->device, get_status_str(get_global_status()), state_str);
+	len = snprintf(
+		cmd, sizeof(cmd),
+		"export INTERFACE=\"%s\"; export DEVICE=\"%s\"; export GLOBAL=\"%s\"; "
+		"for hook in /etc/pingcheck/%s.d/*; do [ -r \"$hook\" ] && sh $hook; "
+		"done",
+		pi->name, pi->device, get_status_str(get_global_status()), state_str);
 
 	if (len <= 0 || (unsigned int)len >= sizeof(cmd)) { // error or truncated
 		LOG_ERR("Run scripts commands truncated!");
@@ -66,28 +68,28 @@ static void task_scripts_run(struct runqueue *q, struct runqueue_task *t)
 }
 
 /* runqueue callback when task got cancelled */
-static void task_scripts_cancel(struct runqueue *q, struct runqueue_task *t, int type)
+static void task_scripts_cancel(struct runqueue* q, struct runqueue_task* t,
+								int type)
 {
 	struct scripts_proc* scr = container_of(t, struct scripts_proc, proc.task);
 	LOG_NOTI("'%s' scripts for '%s' cancelled",
-		 scr->state == ONLINE ? "online" : "offline", scr->intf->name);
+			 scr->state == ONLINE ? "online" : "offline", scr->intf->name);
 	runqueue_process_cancel_cb(q, t, type);
 }
 
 /* runqueue callback when task got killed */
-static void task_scripts_kill(struct runqueue *q, struct runqueue_task *t)
+static void task_scripts_kill(struct runqueue* q, struct runqueue_task* t)
 {
 	struct scripts_proc* scr = container_of(t, struct scripts_proc, proc.task);
 	LOG_NOTI("'%s' scripts for '%s' killed",
-		 scr->state == ONLINE ? "online" : "offline", scr->intf->name);
+			 scr->state == ONLINE ? "online" : "offline", scr->intf->name);
 	runqueue_process_kill_cb(q, t);
 }
 
-static const struct runqueue_task_type task_scripts_type = {
-	.run = task_scripts_run,
-	.cancel = task_scripts_cancel,
-	.kill = task_scripts_kill
-};
+static const struct runqueue_task_type task_scripts_type
+	= {.run = task_scripts_run,
+	   .cancel = task_scripts_cancel,
+	   .kill = task_scripts_kill};
 
 /* called by main to request scripts to be run */
 void scripts_run(struct ping_intf* pi, enum online_state state_new)
@@ -112,14 +114,14 @@ void scripts_run(struct ping_intf* pi, enum online_state state_new)
 	 */
 	if (scr_other->proc.task.queued && !scr_other->proc.task.running) {
 		LOG_NOTI("Cancelling obsolete '%s' scripts for '%s'",
-			 scr->state != ONLINE ? "online" : "offline", pi->name);
+				 scr->state != ONLINE ? "online" : "offline", pi->name);
 		runqueue_task_cancel(&scr_other->proc.task, 1);
 	}
 
 	/* don't queue the same scripts twice */
 	if (scr->proc.task.queued || scr->proc.task.running) {
-		LOG_NOTI("'%s' scripts for '%s' already queued or running",
-			 state_str, pi->name);
+		LOG_NOTI("'%s' scripts for '%s' already queued or running", state_str,
+				 pi->name);
 		return;
 	}
 
@@ -132,7 +134,7 @@ void scripts_run(struct ping_intf* pi, enum online_state state_new)
 	runqueue_task_add(&runq, &scr->proc.task, false);
 }
 
-static void task_panic_run(struct runqueue *q, struct runqueue_task *t)
+static void task_panic_run(struct runqueue* q, struct runqueue_task* t)
 {
 	pid_t pid = fork();
 	if (pid < 0) {
@@ -148,7 +150,8 @@ static void task_panic_run(struct runqueue *q, struct runqueue_task *t)
 	/* child process */
 	char cmd[500];
 	int len = snprintf(cmd, sizeof(cmd),
-		       "for hook in /etc/pingcheck/panic.d/*; do [ -r \"$hook\" ] && sh $hook; done");
+					   "for hook in /etc/pingcheck/panic.d/*; do [ -r "
+					   "\"$hook\" ] && sh $hook; done");
 
 	if (len <= 0 || (unsigned int)len >= sizeof(cmd)) { // error or truncated
 		LOG_ERR("Run scripts commands truncated!");

commit 6a06ce51c4235080c1cdbd59a51a898eaa962509

Fix build with kernel 5.15 and musl
Currently, with kernel 5.15 headers and musl building is failing with
redefinition errors due to a conflict between the kernel and musl
headers.

Musl is able to suppress the conflicting kernel header definitions
if they are included after the standard libc ones, however the reformat
in commit 8cee8ac moved these kernel definitions before <netinet/in.h>
and this suppression has stopped working and is breaking the builds.

Solve this by moving the <netinet/in.h> include before the ICMP kernel
include in the icmp.c

Fixes: 8cee8ac5bece ("Add curly braces and reformat")

icmp.c
@@ -14,10 +14,11 @@
  */
 #include "main.h"
 #include <err.h>
+/* keep libc includes before linux headers for musl compatibility */
+#include <netinet/in.h>
 #include <linux/icmp.h>
 #include <linux/ip.h>
 #include <linux/if.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>