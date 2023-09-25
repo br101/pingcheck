000a799fe88ce232cc537305f9c25d646f9a10c5
Bruno Randolf <randolf.bruno@gmail.com>
2023-09-25 08:50:47 +0100

    Fix typo in example-script.sh
    Closes #22

--- example-script.sh
+++ example-script.sh
@@ -2,4 +2,4 @@
 
 # ${INTERFACE} -- logical network interface (e.g. wan) which goes online or offline
 # ${DEVICE} -- physical device (e.g. eth0) which goes online or offline
-# ${GLOBAL} -- global state ONLINE or OFFLINE depending on wether device is online thru other interfaces
+# ${GLOBAL} -- global state ONLINE or OFFLINE depending on whether device is online thru other interfaces
